3c11725ae76244b698f6e171921c77515f2ece38
Asmae <asmae.tribakk@gmail.com>
2025-05-15 12:32:53 +0100

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CatalogCompnentComponent } from '../catalog-compnent/catalog-compnent.component';
+import { ProductDetailsCompnentComponent } from '../product-details-compnent/product-details-compnent.component';
 
-export const routes: Routes = [];
+export const routes: Routes =  [
+    
+    {path : 'catalog', component : CatalogCompnentComponent, title: 'My Catalog products'},
+    {path : 'product-details', component : ProductDetailsCompnentComponent, title: 'Product details'},
+    {path : '', redirectTo : '/home', pathMatch : 'full'},
+
+];
--- src/catalog-compnent/catalog-compnent.component.ts
+++ src/catalog-compnent/catalog-compnent.component.ts
@@ -4,9 +4,10 @@ import { GetDataService } from '../services/get-data.service';
 import { ProductDetailsCompnentComponent } from '../product-details-compnent/product-details-compnent.component'; 
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-catalog-compnent',
-  imports: [ProductDetailsCompnentComponent, CommonModule],
+  imports: [ProductDetailsCompnentComponent, CommonModule,RouterLink, RouterLinkActive],
   templateUrl: './catalog-compnent.component.html',
   styleUrl: './catalog-compnent.component.css',
   standalone: true
@@ -14,16 +15,32 @@ import { HttpClient } from '@angular/common/http';
 export class CatalogCompnentComponent implements OnInit {
   products: Product[] = [];
   selectedProduct!: Product;
+  filter: string = '';
 
-  constructor(private getDataService: GetDataService) {}
+  constructor(private getDataService: GetDataService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
     this.getDataService.getProducts().subscribe((data: Product[]) => {
       this.products = data;
-    });
+    })
+    this.route.queryParams.subscribe((params)=>{
+      this.filter = params['filter'] ?? '';
+    })
   }
 
   voirDetails(product: Product) {
     this.selectedProduct = product;
   }
+  getFilteredProducts() {
+    return this.filter === ''
+      ? this.products
+      : this.products.filter(
+        (product: any) => product.category === this.filter
+      );
+  }
+  
+
 }
